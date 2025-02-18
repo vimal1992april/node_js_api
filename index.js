f4b43dce258131bd295411497b65c1b31d33fd07
vimal kumar <vimal1992april@gmail.com>
2025-02-18 08:36:17 +0530

    user create using post

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
 
+app.use(express.urlencoded({ extended: false }))
 //routes here
 app.get('/users', (req, res) => {
 	const html = `
@@ -16,25 +18,26 @@ app.get('/api/users', (req, res) => {
 	return res.json(users);
 })
 
-app.get('/api/users/:id', (req, res) => {
+app.route("/api/users/:id")
+.get((req,res) => {
 	const id = Number(req.params.id);
 	const user = users.find((user) => user.id === id);
 	return res.json(user); 
 })
-
-//edit the user
-app.post('/api/users', (req,res) => {
+.post((req,res) => {
+	const body = req.body;
+	users.push({ ...body, id: users.length + 1 });
+	fs.writeFile("./MOCK_DATA.json",JSON.stringify(users), (err, data) => {
+		return res.json({ status: users.last});
+	})
+	
+})
+.patch((req,res) => {
+	return res.json({ status: "peniding"});
+})
+.delete((req,res) => {
 	return res.json({ status: "peniding"});
 })
 
-//edit the user
-app.patch('/api/users/:id', (req,res) => {
-	return res.json({ status: "peniding"});
-})
-
-//edit the user
-app.delete('/api/users/:id', (req,res) => {
-	return res.json({ status: "peniding"});
-})
 
 app.listen(PORT, () => console.log("server startedd at port "+PORT))
